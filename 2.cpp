8f190f891553c3e3081778ae848fa4324e8b95b2
dibkb <59993782+dibkb@users.noreply.github.com>
2022-09-05 15:37:33 +0000

    second file "2"

---
+++ 2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+int main(){
+  std::cout<< "hello C++ compiler" <<"\n";
+  return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+namespace first {
+  int x  = 36;
+}
+namespace second {
+  double x = 36.36;
+}
 int main() {
-	std::cout << "Hello, world!";
+	// std::cout << "This is my first line of code in C++"<<'\n';
+	// std::cout << "This is my second line of code in C++"<<'\n';
+	// int x;
+	// x = 25;
+	// int y = 3;
+	// double sum = x + y;
+	// std::cout<<"The sum of x and y is "<<sum;
+  // const double PI = 3.14;
+  // double radius = 3;
+  // double area = PI * radius * radius;
+  // std::cout<< "Area of the circle with radius 3cm is " << area << " sq. cm";
+  using namespace second;
+  std::cout<< "Standard x "<<x;
 	return 0;
 }
